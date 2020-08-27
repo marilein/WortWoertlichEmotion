2c16503f92be1a9aada4127bde43b1e92e92dddc
Mariam Hemmer <mhemmer@audeering.com>
2020-08-27 19:45:34 +0200

    deleted test files

--- Plotter.py
+++ Plotter.py
@@ -2,5 +2,5 @@ import seaborn as sns
 import data_processor as dtp
 
 
-base_path = 'raw_data/final_data/'
+base_path = 'normalized_data/final_data/'
 experiment_files = dtp.get_filelist_in_folder(base_path)
